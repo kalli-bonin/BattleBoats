39928d38d6a78d6eb8ed1f05616e2cd95f5c58cd
Tanya <tanyaqnguyen@hotmail.com>
2017-06-23 01:17:46 -0400

    instructions

--- KB_Battleship/KB_Battleship/Form1.Designer.cs
+++ KB_Battleship/KB_Battleship/Form1.Designer.cs
@@ -3319,9 +3319,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(16, 68);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(202, 13);
+            this.label2.Size = new System.Drawing.Size(144, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "write out instructions here when complete";
+            this.label2.Text = "Please see attached manual.";
             // 
             // label1
             // 
